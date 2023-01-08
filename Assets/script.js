afef529cbdd988595edffe46e61026334cb142cb
Kole Dallam <kolejasonD@gmail.com>
2023-01-08 13:37:06 -0700

    fixing the set status class and adding Q's

--- Assets/script.js
+++ Assets/script.js
@@ -8,6 +8,10 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
     console.log('Started')
@@ -45,10 +49,73 @@ function resetState() {
     }
 }
 
-function selectAnswer() {
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
 }
 
 const questions = [
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '22', correct: false}
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '22', correct: false}
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '22', correct: false}
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '22', correct: false}
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '22', correct: false}
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '22', correct: false}
+        ]
+    },
     {
         question: 'What is 2 + 2',
         answers: [
